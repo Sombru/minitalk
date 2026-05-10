b7693205721f6442a55c45e256745b59cb3d8660
Sombru <142216472+Sombru@users.noreply.github.com>
2026-05-10 22:08:43 +0200

    byte decoding

--- client.c
+++ client.c
@@ -26,12 +26,50 @@
 //    void     (*sa_restorer)(void);
 // };
 
-int main(int argc, char** argv)
+// decoding bytes
+	// unsigned char c = 0b00110000;
+	// unsigned char res = 0;
+
+	// for (int i = 0; i < 8; ++i)
+	// {
+	// 	res <<= 1; // make room for next bit
+
+	// 	if (c & 0b10000000)
+	// 	{
+	// 		printf("1\n");
+	// 		res |= 1; // insert bit
+	// 	}
+	// 	else
+	// 	{
+	// 		printf("0\n");
+	// 	}
+
+	// 	c <<= 1;
+	// }
+
+int main(int argc, char **argv)
 {
+	size_t i;
+	int k;
+
+	i = 0;
+	k = 0;
 	if (argc != 3)
 		return ft_printf("Error: bad number of arguments\n");
-	kill(ft_atoi(argv[1]), SIGUSR1);
-	printf("%s\n", argv[2]);
+	while (i < ft_strlen(argv[2]))
+	{
+		k = 0;
+		while (k < 8)
+		{
+			if (argv[2][i] & 0b10000000)
+				kill(ft_atoi(argv[1]), SIGUSR1);
+			else
+				kill(ft_atoi(argv[1]), SIGUSR2);
+			argv[2][i] <<= 1;
+			++k;
+		}
+		++i;
+	}
 	
-	
+	printf("message sennt\n");
 }
--- server.c
+++ server.c
@@ -4,16 +4,27 @@
 #include <stdlib.h>
 #include "libft.h"
 
-void handle_usr_sig(int sig)
+void handle_usr_sig1(int sig)
 {
-	ft_printf("recieved signal: %d\n", sig);
+	(void)sig;
+	ft_printf("1\n");
 }
+
+void handle_usr_sig2(int sig)
+{
+	(void)sig;
+	ft_printf("0\n");
+}
+
 
 int main()
 {
-	struct sigaction sa = {0};
-	sa.sa_handler = handle_usr_sig;
-	sigaction(SIGUSR1, &sa, NULL);
+	struct sigaction sa1;
+	struct sigaction sa2;
+	sa1.sa_handler = handle_usr_sig1;
+	sa2.sa_handler = handle_usr_sig2;
+	sigaction(SIGUSR1, &sa1, NULL);
+	sigaction(SIGUSR2, &sa2, NULL); 
 	ft_printf("pid: %d\n", getpid());
 	while (1)
 	{
